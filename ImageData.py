d1200dc24f4978fd94bb6e960c86ff053227e760
ingop <ingo.pastl@gmail.com>
2018-07-14 22:18:35 -0300

    Initial commit

---
+++ ImageData.py
@@ -0,0 +1,4 @@
+class ImageData:
+    def __init__(self, classif, characteristics):
+        self.classification = classif
+        self.characteristics_array = characteristics
---
+++ main.py
@@ -0,0 +1,38 @@
+from skimage.feature import hog
+from skimage.io import imread
+from skimage.transform import resize
+from glob import glob
+from ImageData import ImageData
+
+# Processa todas as imagens em uma determinada pasta e retorna uma lista de objetos ImageData, onde cada objeto contem a lista de caracteristicas e a classificação de uma imagem
+def folder_processing(classification, folder_path):
+    data_list = []
+    for filename in glob(folder_path + '\\**'):
+        image = imread(filename)
+        resized_image = resize(image, (80, 80), anti_aliasing=True)
+
+        # print('Processing image ' + filename)
+        histograms_data = hog(resized_image, orientations=8, pixels_per_cell=(8, 8), cells_per_block=(1, 1), feature_vector=True)
+        data_list.append(ImageData(classification, histograms_data))
+    return data_list
+
+# Retorna uma lista com todas as imagens de treinamento já processadas
+def get_training_elements():
+    map = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9",
+           10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F", 16: "G", 17: "H", 18: "I",
+           19: "J", 20: "K", 21: "L", 22: "M", 23: "N", 24: "O", 25: "P", 26: "Q", 27: "R",
+           28: "S", 29: "T", 30: "U", 31: "V", 32: "W", 33: "X", 34: "Y", 35: "Z"}
+
+    elements_list = []
+    count = 0
+    for path in glob('characters\\**'):
+        if (count == 0):
+            elements_list = elements_list + folder_processing('0', path)
+            count += 1
+        elif (count == 1):
+            elements_list = elements_list + folder_processing('1', path)
+            count += 1
+    return elements_list
+
+elements_list = get_training_elements()
+# print(elements_list)
