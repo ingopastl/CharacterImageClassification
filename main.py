a3f00c3bc412d2277ae8c487b306e8246cd8115a
ingop <ingo.pastl@gmail.com>
2018-07-14 22:35:50 -0300

    Defini de fato o método de classificação.

--- main.py
+++ main.py
@@ -21,17 +21,16 @@ def get_training_elements():
     map = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9",
            10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F", 16: "G", 17: "H", 18: "I",
            19: "J", 20: "K", 21: "L", 22: "M", 23: "N", 24: "O", 25: "P", 26: "Q", 27: "R",
-           28: "S", 29: "T", 30: "U", 31: "V", 32: "W", 33: "X", 34: "Y", 35: "Z"}
+           28: "S", 29: "T", 30: "U", 31: "V", 32: "W", 33: "X", 34: "Y", 35: "Z", 36: "a",
+           37: "b", 38: "c", 39: "d", 40: "e", 41: "f", 42: "g", 43: "h", 44: "i", 45: "j",
+           46: "k", 47: "l", 48: "m", 49: "n", 50: "o", 51: "p", 52: "q", 53: "r", 54: "s",
+           55: "t", 56: "u", 57: "v", 58: "w", 59: "x", 60: "y", 62: "z"}
 
     elements_list = []
     count = 0
     for path in glob('characters\\**'):
-        if (count == 0):
-            elements_list = elements_list + folder_processing('0', path)
-            count += 1
-        elif (count == 1):
-            elements_list = elements_list + folder_processing('1', path)
-            count += 1
+        elements_list = elements_list + folder_processing(map[count], path)
+        count += 1
     return elements_list
 
 elements_list = get_training_elements()
