96eac113cdc5df68c62fc43692513d0371bae49e
ingopastl <ingo.pastl@gmail.com>
2018-07-25 09:13:33 -0300

    Melhoria nos comentários.

--- main.py
+++ main.py
@@ -10,9 +10,9 @@ from classes import ImageData
 from classes import ImageDistance
 
 '''
-Processa todas as imagens em uma determinada pasta e retorna uma lista de objetos ImageData, onde cada objeto contem a 
-lista de caracteristicas e a classificação de uma imagem.
-Além de retornar uma lista, a função também escreve os arrays em um arquivo txt
+Processa todas as imagens em uma determinada pasta e retorna uma lista de objetos ImageData, onde cada objeto contem o 
+array de caracteristicas e a classificação de uma imagem.
+Além de retornar uma lista, a função também escreve os arrays e a classificação em um arquivo txt
 '''
 
 
@@ -36,12 +36,12 @@ Retorna uma lista com todas as imagens da pasta "characters" processadas e trans
 
 def create_database_elements():
     mapping = {0: "0", 1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9",
-              10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F", 16: "G", 17: "H", 18: "I",
-              19: "J", 20: "K", 21: "L", 22: "M", 23: "N", 24: "O", 25: "P", 26: "Q", 27: "R",
-              28: "S", 29: "T", 30: "U", 31: "V", 32: "W", 33: "X", 34: "Y", 35: "Z", 36: "a",
-              37: "b", 38: "c", 39: "d", 40: "e", 41: "f", 42: "g", 43: "h", 44: "i", 45: "j",
-              46: "k", 47: "l", 48: "m", 49: "n", 50: "o", 51: "p", 52: "q", 53: "r", 54: "s",
-              55: "t", 56: "u", 57: "v", 58: "w", 59: "x", 60: "y", 61: "z"}
+               10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F", 16: "G", 17: "H", 18: "I",
+               19: "J", 20: "K", 21: "L", 22: "M", 23: "N", 24: "O", 25: "P", 26: "Q", 27: "R",
+               28: "S", 29: "T", 30: "U", 31: "V", 32: "W", 33: "X", 34: "Y", 35: "Z", 36: "a",
+               37: "b", 38: "c", 39: "d", 40: "e", 41: "f", 42: "g", 43: "h", 44: "i", 45: "j",
+               46: "k", 47: "l", 48: "m", 49: "n", 50: "o", 51: "p", 52: "q", 53: "r", 54: "s",
+               55: "t", 56: "u", 57: "v", 58: "w", 59: "x", 60: "y", 61: "z"}
 
     file = io.open("data.txt", "w")
     image_data_list = []
